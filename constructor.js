6f3febef26c4634e0fead156e6c4216d03234f72
CryptoFallen <richardtom1203@protonmail.com>
2023-05-19 15:08:41 +0100

    Added a try-catch block around the processFile function to catch any unexpected errors and log them to the console, also refactored code to catch errors first

--- constructor.js
+++ constructor.js
@@ -1,55 +1,61 @@
-const fs = require('fs');
-const path = require('path');
-const readline = require('readline');
-const colors = require('colors');
+const fs = require("fs");
+const path = require("path");
+const readline = require("readline");
+const colors = require("colors");
 
 function findConstructors(filePath) {
-  if (fs.existsSync(filePath)) {
-    const stat = fs.statSync(filePath);
-    if (stat.isDirectory()) {
-      const files = fs.readdirSync(filePath);
-      const solFiles = files.filter((file) => file.endsWith('.sol'));
-      solFiles.forEach((file) => {
-        const fullPath = path.join(filePath, file);
-        processFile(fullPath);
-      });
-    } else {
-      processFile(filePath);
-    }
+  if (!fs.existsSync(filePath)) {
+    console.log("File or directory does not exist.");
+    return;
+  }
+
+  const stat = fs.statSync(filePath);
+
+  if (stat.isDirectory()) {
+    const files = fs.readdirSync(filePath);
+    const solFiles = files.filter((file) => file.endsWith(".sol"));
+    solFiles.forEach((file) => {
+      const fullPath = path.join(filePath, file);
+      processFile(fullPath);
+    });
   } else {
-    console.log('File or directory does not exist.');
+    processFile(filePath);
   }
 }
 
 function processFile(file) {
-  const rl = readline.createInterface({
-    input: fs.createReadStream(file),
-    crlfDelay: Infinity,
-  });
+  try {
+    const rl = readline.createInterface({
+      input: fs.createReadStream(file),
+      crlfDelay: Infinity,
+    });
 
-  let contents = '';
+    let contents = "";
 
-  rl.on('line', (line) => {
-    contents += line + '\n';
-  });
+    rl.on("line", (line) => {
+      contents += line + "\n";
+    });
 
-  rl.on('close', () => {
-    const regex = /constructor\s*\((.*?)\)\s*\{(.*?)\}/gs;
-    let matches;
-    while ((matches = regex.exec(contents)) !== null) {
-      const args = matches[1].trim();
-      const code = matches[2].trim();
-      console.log(colors.green(`Constructor in ${path.basename(file)}:`));
-      if (code.includes('== address(0)')) {
-        console.log(colors.red('Potential 0 address check exists'));
-      } else {
-        console.log(colors.red('No checks for 0 address exist'));
+    rl.on("close", () => {
+      const regex = /constructor\s*\((.*?)\)\s*\{(.*?)\}/gs;
+      let matches;
+      while ((matches = regex.exec(contents)) !== null) {
+        const args = matches[1].trim();
+        const code = matches[2].trim();
+        console.log(colors.green(`Constructor in ${path.basename(file)}:`));
+        if (code.includes("== address(0)")) {
+          console.log(colors.red("Potential 0 address check exists"));
+        } else {
+          console.log(colors.red("No checks for 0 address exist"));
+        }
+        console.log(colors.yellow(`Arguments: ${args}`));
+        console.log(colors.blue(`Code: ${code}`));
+        console.log("=".repeat(30));
       }
-      console.log(colors.yellow(`Arguments: ${args}`));
-      console.log(colors.blue(`Code: ${code}`));
-      console.log('='.repeat(30));
-    }
-  });
+    });
+  } catch (err) {
+    console.error("Unexpected error:", err);
+  }
 }
 
 const rl = readline.createInterface({
@@ -57,8 +63,7 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
-rl.question('Enter file path or folder: ', (path) => {
+rl.question("Enter file path or folder: ", (path) => {
   findConstructors(path);
   rl.close();
 });
-
